cd7434b5fbe22c42c8013ba6f6d5abe76e4fe61d
Tai Dang <tai.dangit@gmail.com>
2018-11-12 21:01:36 -0700

    Assignment 6 Component Album: Completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import albumData from './../data/albums';
+import React, { Component } from "react";
+import albumData from "./../data/albums";
 
 class Album extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
-    const album = albumData.find( album => {
-      return album.slug === this.props.match.params.slug
+    const album = albumData.find(album => {
+      return album.slug === this.props.match.params.slug;
     });
 
     this.state = {
@@ -14,33 +14,41 @@ class Album extends Component {
     };
   }
 
-  render() { 
+  render() {
     return (
       <section className="album">
         {this.props.match.params.slug} Album will go here
         <section id="album-info">
-           <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title} />
-           <div className="album-details">
-             <h1 id="album-title">{this.state.album.title}</h1>
-             <h2 className="artist">{this.state.album.artist}</h2>
-             <div id="release-info">{this.state.album.releaseInfo}</div>
-           </div>
-         </section>
-         
+          <img
+            id="album-cover-art"
+            src={this.state.album.albumCover}
+            alt={this.state.album.title}
+          />
+          <div className="album-details">
+            <h1 id="album-title">{this.state.album.title}</h1>
+            <h2 className="artist">{this.state.album.artist}</h2>
+            <div id="release-info">{this.state.album.releaseInfo}</div>
+          </div>
+        </section>
         <table id="song-list">
           <colgroup>
             <col id="song-number-column" />
             <col id="song-title-column" />
-            <col id="song-duration-column" /> 
+            <col id="song-duration-column" />
           </colgroup>
-          <tbody>
-            
+          <tbody className="album-songs">
+            {this.state.album.songs.map((song, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{song.title}</td>
+                <td>{song.duration}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
-
       </section>
     );
   }
 }
- 
+
 export default Album;
