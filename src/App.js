c5d885090dbfd37b7814430dac3bed90a1951bae
Tai Dang <tai.dangit@gmail.com>
2019-08-01 21:11:09 -0700

    minor update

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ class App extends Component {
         </main>
 
         <footer>
-          <p>Jamify music player &copy; 2018</p>
+          <p>Jamify music player &copy; 2019</p>
         </footer>
       </div>
     );
